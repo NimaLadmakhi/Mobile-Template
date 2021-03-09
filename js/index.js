153ee6986b77d6453ff009fcb9fa683117e808b6
cody <cody18v@gmail.com>
2021-03-09 01:24:39 -0800

    add animation

--- js/index.js
+++ js/index.js
@@ -20,4 +20,37 @@ $( document ).ready( function () {
       }
     }
   } );
+  $( ".header__icon" ).click( function () {
+    $( ".header__list" ).toggleClass( 'header__list--hide' );
+  } );
+
+  const scrollReveal = new ScrollReveal();
+  scrollReveal.reveal( '.banner__title', makeReveal( 'top' ) );
+  scrollReveal.reveal( '.banner__desc', makeReveal( 'left' ) );
+  scrollReveal.reveal( '.banner__button', makeReveal( 'bottom' ) );
+  scrollReveal.reveal( '.banner__image', makeReveal( 'right' ) );
+  scrollReveal.reveal( '.header__logo', makeReveal( 'left' ) );
+  scrollReveal.reveal( '.header__item', makeReveal( 'right' ) );
+  scrollReveal.reveal( '.services__card--first', makeReveal( 'right' ) );
+  scrollReveal.reveal( '.services__card--second', makeReveal( 'bottom' ) );
+  scrollReveal.reveal( '.services__card--third', makeReveal( 'left' ) );
+  scrollReveal.reveal( '.about__container-img', makeReveal( 'left' ) );
+  scrollReveal.reveal( '.about__card--one', makeReveal( 'right' ) );
+  scrollReveal.reveal( '.about__card--two', makeReveal( 'right', 500 ) );
+  scrollReveal.reveal( '.about__card--three', makeReveal( 'right', 1000 ) );
+  scrollReveal.reveal( '.sliding', makeReveal( 'right' ) );
+  scrollReveal.reveal( '.footer__form', makeReveal( 'left' ) );
+  scrollReveal.reveal( '.footer__body', makeReveal( 'right', 500 ) );
+  scrollReveal.reveal( '.footer__item--first a', makeReveal( 'bottom', 1000 ) );
+  scrollReveal.reveal( '.footer__item--second a', makeReveal( 'top', 1000 ) );
+  scrollReveal.reveal( '.footer__item--third a', makeReveal( 'bottom', 1000 ) );
+  scrollReveal.reveal( '.footer__item--fourth a', makeReveal( 'top', 1000 ) );
+  scrollReveal.reveal( '.footer__item--fiveth a', makeReveal( 'bottom', 1000 ) );
+  scrollReveal.reveal( '.footer__end span', makeReveal( 'bottom', 2000 ) );
 } );
+
+
+function makeReveal( origin, delay = 0 ) {
+  return { origin, duration: 2000, reset: true, distance: '50px', delay };
+}
+
