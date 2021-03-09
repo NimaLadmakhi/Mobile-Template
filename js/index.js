cf709623127b9b17f3e5fd2b7db8b192bd9bf42f
cody <cody18v@gmail.com>
2021-03-08 21:38:52 -0800

    the add image to sliding and complete sliding by owl carousel

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$( document ).ready( function () {
+  const owlCarousel = $( ".owl-carousel" );
+  $( '.owl__btn--next' ).click( function () {
+    owlCarousel.trigger( 'next.owl.carousel' );
+  } )
+  $( '.owl__btn--prev' ).click( function () {
+    owlCarousel.trigger( 'prev.owl.carousel' );
+  } )
+  owlCarousel.owlCarousel( {
+    loop: true,
+    items: 2,
+    margin: 5,
+    responsive: {
+      300: {
+        items: 1
+      },
+      1000: {
+        items: 2,
+        margin: 0,
+      }
+    }
+  } );
+} );
