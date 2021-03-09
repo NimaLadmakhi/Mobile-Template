0ecaee03ca8aa2a1c0e566a5a41707ca9d0c10f4
cody <cody18v@gmail.com>
2021-03-09 01:29:37 -0800

    fix some bugs

--- js/index.js
+++ js/index.js
@@ -24,29 +24,31 @@ $( document ).ready( function () {
     $( ".header__list" ).toggleClass( 'header__list--hide' );
   } );
 
-  const scrollReveal = new ScrollReveal();
-  scrollReveal.reveal( '.banner__title', makeReveal( 'top' ) );
-  scrollReveal.reveal( '.banner__desc', makeReveal( 'left' ) );
-  scrollReveal.reveal( '.banner__button', makeReveal( 'bottom' ) );
-  scrollReveal.reveal( '.banner__image', makeReveal( 'right' ) );
-  scrollReveal.reveal( '.header__logo', makeReveal( 'left' ) );
-  scrollReveal.reveal( '.header__item', makeReveal( 'right' ) );
-  scrollReveal.reveal( '.services__card--first', makeReveal( 'right' ) );
-  scrollReveal.reveal( '.services__card--second', makeReveal( 'bottom' ) );
-  scrollReveal.reveal( '.services__card--third', makeReveal( 'left' ) );
-  scrollReveal.reveal( '.about__container-img', makeReveal( 'left' ) );
-  scrollReveal.reveal( '.about__card--one', makeReveal( 'right' ) );
-  scrollReveal.reveal( '.about__card--two', makeReveal( 'right', 500 ) );
-  scrollReveal.reveal( '.about__card--three', makeReveal( 'right', 1000 ) );
-  scrollReveal.reveal( '.sliding', makeReveal( 'right' ) );
-  scrollReveal.reveal( '.footer__form', makeReveal( 'left' ) );
-  scrollReveal.reveal( '.footer__body', makeReveal( 'right', 500 ) );
-  scrollReveal.reveal( '.footer__item--first a', makeReveal( 'bottom', 1000 ) );
-  scrollReveal.reveal( '.footer__item--second a', makeReveal( 'top', 1000 ) );
-  scrollReveal.reveal( '.footer__item--third a', makeReveal( 'bottom', 1000 ) );
-  scrollReveal.reveal( '.footer__item--fourth a', makeReveal( 'top', 1000 ) );
-  scrollReveal.reveal( '.footer__item--fiveth a', makeReveal( 'bottom', 1000 ) );
-  scrollReveal.reveal( '.footer__end span', makeReveal( 'bottom', 2000 ) );
+  if ( window.innerWidth > 1000 ) {
+    const scrollReveal = new ScrollReveal();
+    scrollReveal.reveal( '.banner__title', makeReveal( 'top' ) );
+    scrollReveal.reveal( '.banner__desc', makeReveal( 'left' ) );
+    scrollReveal.reveal( '.banner__button', makeReveal( 'bottom' ) );
+    scrollReveal.reveal( '.banner__image', makeReveal( 'right' ) );
+    scrollReveal.reveal( '.header__logo', makeReveal( 'left' ) );
+    scrollReveal.reveal( '.header__item', makeReveal( 'right' ) );
+    scrollReveal.reveal( '.services__card--first', makeReveal( 'right' ) );
+    scrollReveal.reveal( '.services__card--second', makeReveal( 'bottom' ) );
+    scrollReveal.reveal( '.services__card--third', makeReveal( 'left' ) );
+    scrollReveal.reveal( '.about__container-img', makeReveal( 'left' ) );
+    scrollReveal.reveal( '.about__card--one', makeReveal( 'right' ) );
+    scrollReveal.reveal( '.about__card--two', makeReveal( 'right', 500 ) );
+    scrollReveal.reveal( '.about__card--three', makeReveal( 'right', 1000 ) );
+    scrollReveal.reveal( '.sliding', makeReveal( 'right' ) );
+    scrollReveal.reveal( '.footer__form', makeReveal( 'left' ) );
+    scrollReveal.reveal( '.footer__body', makeReveal( 'right', 500 ) );
+    scrollReveal.reveal( '.footer__item--first a', makeReveal( 'bottom', 1000 ) );
+    scrollReveal.reveal( '.footer__item--second a', makeReveal( 'top', 1000 ) );
+    scrollReveal.reveal( '.footer__item--third a', makeReveal( 'bottom', 1000 ) );
+    scrollReveal.reveal( '.footer__item--fourth a', makeReveal( 'top', 1000 ) );
+    scrollReveal.reveal( '.footer__item--fiveth a', makeReveal( 'bottom', 1000 ) );
+    scrollReveal.reveal( '.footer__end span', makeReveal( 'bottom', 2000 ) );
+  }
 } );
 
 
